1d0a71ebab057e0972cef96d5ba8aa3a1913649c
fatmanur.ozel <fatmanurozel@outlook.com>
2021-12-13 17:43:29 +0300

    product data list

--- App.js
+++ App.js
@@ -1,21 +1,63 @@
-import { StatusBar } from 'expo-status-bar';
-import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState, useEffect } from 'react';
+import { Text, View, StyleSheet, FlatList, TextInput } from 'react-native';
+import Constants from 'expo-constants';
 
 export default function App() {
+  const [products, setProducts] = useState([]);
+
+  useEffect(() => {
+    fetch('https://northwind.vercel.app/api/products')
+      .then((res) => res.json())
+      .then((data) => {
+        setProducts(data);
+      });
+  }, []);
+
+  const searchProducts = (name) => {
+    fetch('https://northwind.vercel.app/api/products')
+      .then((res) => res.json())
+      .then((data) => {
+        let filteredProducts = data.filter((q) =>
+          q.name.toLowerCase().includes(name.toLowerCase())
+        );
+        setProducts(filteredProducts);
+      });
+  };
+
+  const renderItem = ({ item }) => (
+    <Text style={{ padding: 20, backgroundColor: 'gray', margin: 3 }}>
+      {item.name} 
+    </Text>
+  );
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+      <TextInput
+        style={styles.input}
+        onChangeText={searchProducts}
+        placeholder="Product Name" />
+
+      <FlatList
+        data={products}
+        renderItem={renderItem}
+        keyExtractor={(item) => item.id}></FlatList>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
+  input: {
+    height: 40,
+    margin: 12,
+    borderWidth: 1,
+    padding: 10,
+  },
   container: {
     flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
     justifyContent: 'center',
+    paddingTop: Constants.statusBarHeight,
+    backgroundColor: '#ecf0f1',
+    padding: 8,
   },
 });
+
