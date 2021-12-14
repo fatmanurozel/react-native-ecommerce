ad4a3560e0057398ee9bde66130be46f54484e0b
Fatih Mustafa Sağır <fms@hotmail.com.tr>
2021-12-14 16:12:15 +0300

    navigation

--- App.js
+++ App.js
@@ -4,13 +4,23 @@ import Suppliers from "./src/SuppliersList";
 import Products from "./src/ProductsList";
 import Categories from "./src/CategoriesList";
 
+import { NavigationContainer } from "@react-navigation/native";
+
+import { createBottomTabNavigator } from "@react-navigation/bottom-tabs";
+
+const Tab = createBottomTabNavigator();
+
 function App() {
   return (
     <>
-      <Categories />
-      <Products />
-      <Suppliers />
-      <Orders />
+      <NavigationContainer>
+        <Tab.Navigator initialRouteName="Categories">
+          <Tab.Screen name="Categories" component={Categories} />
+          <Tab.Screen name="Products" component={Products} />
+          <Tab.Screen name="Suppliers" component={Suppliers} />
+          <Tab.Screen name="Orders" component={Orders} />
+        </Tab.Navigator>
+      </NavigationContainer>
     </>
   );
 }
