de06660570693ae3187fd1f27d22ca569d80534c
Fatih Mustafa Sağır <fms@hotmail.com.tr>
2021-12-14 15:12:06 +0300

    category-list

--- App.js
+++ App.js
@@ -2,10 +2,12 @@ import React from "react";
 import Orders from "./src/OrdersList";
 import Suppliers from "./src/SuppliersList";
 import Products from "./src/ProductsList";
+import CategoriesList from "./src/CategoriesList";
 
 function App() {
   return (
     <div>
+      <CategoriesList />
       <Products />
       <Suppliers />
       <Orders />
