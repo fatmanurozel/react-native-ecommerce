9bc1308e26be6c9284f6f59684951f2948983e37
rkymtc <rukiyemotcu@gmail.com>
2021-12-14 14:31:17 +0300

    suppliers

--- App.js
+++ App.js
@@ -1,42 +1,27 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, StyleSheet, FlatList, TextInput } from 'react-native';
-import Constants from 'expo-constants';
+import { Text, View, FlatList} from 'react-native';
+
 
 export default function App() {
   const [products, setProducts] = useState([]);
 
   useEffect(() => {
-    fetch('https://northwind.vercel.app/api/products')
+    fetch('https://northwind.vercel.app/api/suppliers')
       .then((res) => res.json())
       .then((data) => {
         setProducts(data);
       });
   }, []);
 
-  const searchProducts = (name) => {
-    fetch('https://northwind.vercel.app/api/products')
-      .then((res) => res.json())
-      .then((data) => {
-        let filteredProducts = data.filter((q) =>
-          q.name.toLowerCase().includes(name.toLowerCase())
-        );
-        setProducts(filteredProducts);
-      });
-  };
-
+  
   const renderItem = ({ item }) => (
-    <Text style={{ padding: 20, backgroundColor: 'gray', margin: 3 }}>
+    <Text>
       {item.name} 
     </Text>
   );
 
   return (
-    <View style={styles.container}>
-      <TextInput
-        style={styles.input}
-        onChangeText={searchProducts}
-        placeholder="Product Name" />
-
+    <View>
       <FlatList
         data={products}
         renderItem={renderItem}
@@ -45,19 +30,4 @@ export default function App() {
   );
 }
 
-const styles = StyleSheet.create({
-  input: {
-    height: 40,
-    margin: 12,
-    borderWidth: 1,
-    padding: 10,
-  },
-  container: {
-    flex: 1,
-    justifyContent: 'center',
-    paddingTop: Constants.statusBarHeight,
-    backgroundColor: '#ecf0f1',
-    padding: 8,
-  },
-});
 
